473ae1c3a4654576170165dd435eae6182696ee9
Arnab Ahamed Siam <siamarnab@gmail.com>
2023-11-15 19:34:49 +0600

    reverting changes on facefusion ui

--- extensions/sd-face-fusion-extension/facefusion/uis/core.py
+++ extensions/sd-face-fusion-extension/facefusion/uis/core.py
@@ -1,260 +1,130 @@
-import os
-
-os.environ['OMP_NUM_THREADS'] = '1'
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-import signal
+from typing import Dict, Optional, Any, List
+from types import ModuleType
+import importlib
 import sys
-import warnings
-import platform
-import shutil
-import onnxruntime
-import tensorflow
-from argparse import ArgumentParser, HelpFormatter
+import gradio
 
-import facefusion.choices
 import facefusion.globals
 from facefusion import metadata, wording
-from facefusion.predictor import predict_image, predict_video
-from facefusion.processors.frame.core import get_frame_processors_modules, load_frame_processor_module
-from facefusion.utilities import is_image, is_video, detect_fps, compress_image, merge_video, extract_frames, get_temp_frame_paths, restore_audio, create_temp, move_temp, clear_temp, list_module_names, encode_execution_providers, decode_execution_providers, normalize_output_path
+from facefusion.uis.typing import Component, ComponentName
+from facefusion.utilities import resolve_relative_path
 
-warnings.filterwarnings('ignore', category = FutureWarning, module = 'insightface')
-warnings.filterwarnings('ignore', category = UserWarning, module = 'gradio')
-warnings.filterwarnings('ignore', category = UserWarning, module = 'torchvision')
-
-def get_argument_parser():
-	signal.signal(signal.SIGINT, lambda signal_number, frame: destroy())
-	program = ArgumentParser(formatter_class = lambda prog: HelpFormatter(prog, max_help_position = 120), add_help = False)
-	# general
-	program.add_argument('-s', '--source', help = wording.get('source_help'), dest = 'source_path')
-	program.add_argument('-t', '--target', help = wording.get('target_help'), dest = 'target_path')
-	program.add_argument('-o', '--output', help = wording.get('output_help'), dest = 'output_path')
-	program.add_argument('-v', '--version', version = metadata.get('name') + ' ' + metadata.get('version'), action = 'version')
-	# misc
-	group_misc = program.add_argument_group('misc')
-	group_misc.add_argument('--skip-download', help = wording.get('skip_download_help'), dest = 'skip_download', default = False, action = 'store_true')
-	group_misc.add_argument('--headless', help = wording.get('headless_help'), dest = 'headless', action = 'store_true')
-	# execution
-	group_execution = program.add_argument_group('execution')
-	group_execution.add_argument('--execution-providers', help = wording.get('execution_providers_help').format(choices = 'cpu'), dest = 'execution_providers', default = [ 'cuda' ], choices = encode_execution_providers(onnxruntime.get_available_providers()), nargs = '+')
-	group_execution.add_argument('--execution-thread-count', help = wording.get('execution_thread_count_help'), dest = 'execution_thread_count', type = int, default = 1)
-	group_execution.add_argument('--execution-queue-count', help = wording.get('execution_queue_count_help'), dest = 'execution_queue_count', type = int, default = 1)
-	group_execution.add_argument('--max-memory', help=wording.get('max_memory_help'), dest='max_memory', type = int)
-	# face recognition
-	group_face_recognition = program.add_argument_group('face recognition')
-	group_face_recognition.add_argument('--face-recognition', help = wording.get('face_recognition_help'), dest = 'face_recognition', default = 'reference', choices = facefusion.choices.face_recognitions)
-	group_face_recognition.add_argument('--face-analyser-direction', help = wording.get('face_analyser_direction_help'), dest = 'face_analyser_direction', default = 'left-right', choices = facefusion.choices.face_analyser_directions)
-	group_face_recognition.add_argument('--face-analyser-age', help = wording.get('face_analyser_age_help'), dest = 'face_analyser_age', choices = facefusion.choices.face_analyser_ages)
-	group_face_recognition.add_argument('--face-analyser-gender', help = wording.get('face_analyser_gender_help'), dest = 'face_analyser_gender', choices = facefusion.choices.face_analyser_genders)
-	group_face_recognition.add_argument('--reference-face-position', help = wording.get('reference_face_position_help'), dest = 'reference_face_position', type = int, default = 0)
-	group_face_recognition.add_argument('--reference-face-distance', help = wording.get('reference_face_distance_help'), dest = 'reference_face_distance', type = float, default = 1.5)
-	group_face_recognition.add_argument('--reference-frame-number', help = wording.get('reference_frame_number_help'), dest = 'reference_frame_number', type = int, default = 0)
-	# frame extraction
-	group_processing = program.add_argument_group('frame extraction')
-	group_processing.add_argument('--trim-frame-start', help = wording.get('trim_frame_start_help'), dest = 'trim_frame_start', type = int)
-	group_processing.add_argument('--trim-frame-end', help = wording.get('trim_frame_end_help'), dest = 'trim_frame_end', type = int)
-	group_processing.add_argument('--temp-frame-format', help = wording.get('temp_frame_format_help'), dest = 'temp_frame_format', default = 'jpg', choices = facefusion.choices.temp_frame_formats)
-	group_processing.add_argument('--temp-frame-quality', help = wording.get('temp_frame_quality_help'), dest = 'temp_frame_quality', type = int, default = 100, choices = range(101), metavar = '[0-100]')
-	group_processing.add_argument('--keep-temp', help = wording.get('keep_temp_help'), dest = 'keep_temp', action = 'store_true')
-	# output creation
-	group_output = program.add_argument_group('output creation')
-	group_output.add_argument('--output-image-quality', help=wording.get('output_image_quality_help'), dest = 'output_image_quality', type = int, default = 80, choices = range(101), metavar = '[0-100]')
-	group_output.add_argument('--output-video-encoder', help = wording.get('output_video_encoder_help'), dest = 'output_video_encoder', default = 'libx264', choices = facefusion.choices.output_video_encoders)
-	group_output.add_argument('--output-video-quality', help = wording.get('output_video_quality_help'), dest = 'output_video_quality', type = int, default = 100, choices = range(101), metavar = '[0-100]')
-	group_output.add_argument('--keep-fps', help = wording.get('keep_fps_help'), dest = 'keep_fps', default = True, action = 'store_true')
-	group_output.add_argument('--skip-audio', help = wording.get('skip_audio_help'), dest = 'skip_audio', action = 'store_true')
-	# frame processors
-	available_frame_processors = list_module_names('facefusion/processors/frame/modules')
-	program = ArgumentParser(parents = [ program ], formatter_class = program.formatter_class, add_help = True)
-	group_frame_processors = program.add_argument_group('frame processors')
-	group_frame_processors.add_argument('--frame-processors', help = wording.get('frame_processors_help').format(choices = ', '.join(available_frame_processors)), dest = 'frame_processors', default = [ 'face_swapper' ], nargs = '+')
-	for frame_processor in available_frame_processors:
-		frame_processor_module = load_frame_processor_module(frame_processor)
-		frame_processor_module.register_args(group_frame_processors)
-	# uis
-	group_uis = program.add_argument_group('uis')
-	group_uis.add_argument('--ui-layouts', help = wording.get('ui_layouts_help').format(choices = ', '.join(list_module_names('facefusion/uis/layouts'))), dest = 'ui_layouts', default = [ 'default' ], nargs = '+')
-
-	return program
-
-def cli() -> None:
-	run(get_argument_parser())
-
-def apply_args(program : ArgumentParser) -> None:
-	args = program.parse_args([])
-	# general
-	facefusion.globals.source_path = args.source_path
-	facefusion.globals.target_path = args.target_path
-	facefusion.globals.output_path = normalize_output_path(facefusion.globals.source_path, facefusion.globals.target_path, "/home/sduser/stable-diffusion-webui/output")
-	# misc
-	facefusion.globals.skip_download = args.skip_download
-	facefusion.globals.headless = args.headless
-	# execution
-	facefusion.globals.execution_providers = decode_execution_providers(args.execution_providers)
-	facefusion.globals.execution_thread_count = args.execution_thread_count
-	facefusion.globals.execution_queue_count = args.execution_queue_count
-	facefusion.globals.max_memory = args.max_memory
-	# face recognition
-	facefusion.globals.face_recognition = args.face_recognition
-	facefusion.globals.face_analyser_direction = args.face_analyser_direction
-	facefusion.globals.face_analyser_age = args.face_analyser_age
-	facefusion.globals.face_analyser_gender = args.face_analyser_gender
-	facefusion.globals.reference_face_position = args.reference_face_position
-	facefusion.globals.reference_face_distance = args.reference_face_distance
-	facefusion.globals.reference_frame_number = args.reference_frame_number
-	# frame extraction
-	facefusion.globals.trim_frame_start = args.trim_frame_start
-	facefusion.globals.trim_frame_end = args.trim_frame_end
-	facefusion.globals.temp_frame_format = args.temp_frame_format
-	facefusion.globals.temp_frame_quality = args.temp_frame_quality
-	facefusion.globals.keep_temp = args.keep_temp
-	# output creation
-	facefusion.globals.output_image_quality = args.output_image_quality
-	facefusion.globals.output_video_encoder = args.output_video_encoder
-	facefusion.globals.output_video_quality = args.output_video_quality
-	facefusion.globals.keep_fps = args.keep_fps
-	facefusion.globals.skip_audio = args.skip_audio
-	# frame processors
-	available_frame_processors = list_module_names('facefusion/processors/frame/modules')
-	facefusion.globals.frame_processors = args.frame_processors
-	for frame_processor in available_frame_processors:
-		frame_processor_module = load_frame_processor_module(frame_processor)
-		frame_processor_module.apply_args(program)
-	# uis
-	facefusion.globals.ui_layouts = args.ui_layouts
+UI_COMPONENTS: Dict[ComponentName, Component] = {}
+UI_LAYOUT_MODULES : List[ModuleType] = []
+UI_LAYOUT_METHODS =\
+[
+	'pre_check',
+	'pre_render',
+	'render',
+	'listen',
+	'run'
+]
 
 
-def run(program : ArgumentParser) -> None:
-	apply_args(program)
-	limit_resources()
-	if not pre_check():
-		return
-	for frame_processor_module in get_frame_processors_modules(facefusion.globals.frame_processors):
-		if not frame_processor_module.pre_check():
-			return
-	if facefusion.globals.headless:
-		conditional_process()
-	else:
-		import facefusion.uis.core as ui
-
-		for ui_layout in ui.get_ui_layouts_modules(facefusion.globals.ui_layouts):
-			if not ui_layout.pre_check():
-				return
-		ui.launch()
+def load_ui_layout_module(ui_layout : str) -> Any:
+	try:
+		ui_layout_module = importlib.import_module('facefusion.uis.layouts.' + ui_layout)
+		for method_name in UI_LAYOUT_METHODS:
+			if not hasattr(ui_layout_module, method_name):
+				raise NotImplementedError
+	except ModuleNotFoundError:
+		sys.exit(wording.get('ui_layout_not_loaded').format(ui_layout = ui_layout))
+	except NotImplementedError:
+		sys.exit(wording.get('ui_layout_not_implemented').format(ui_layout = ui_layout))
+	return ui_layout_module
 
 
-def destroy() -> None:
-	if facefusion.globals.target_path:
-		clear_temp(facefusion.globals.target_path)
-	sys.exit()
+def get_ui_layouts_modules(ui_layouts : List[str]) -> List[ModuleType]:
+	global UI_LAYOUT_MODULES
+
+	if not UI_LAYOUT_MODULES:
+		for ui_layout in ui_layouts:
+			ui_layout_module = load_ui_layout_module(ui_layout)
+			UI_LAYOUT_MODULES.append(ui_layout_module)
+	return UI_LAYOUT_MODULES
 
 
-def limit_resources() -> None:
-	# prevent tensorflow memory leak
-	gpus = tensorflow.config.experimental.list_physical_devices('GPU')
-	for gpu in gpus:
-		tensorflow.config.experimental.set_virtual_device_configuration(gpu,
-		[
-			tensorflow.config.experimental.VirtualDeviceConfiguration(memory_limit = 1024)
-		])
-	# limit memory usage
-	if facefusion.globals.max_memory:
-		memory = facefusion.globals.max_memory * 1024 ** 3
-		if platform.system().lower() == 'darwin':
-			memory = facefusion.globals.max_memory * 1024 ** 6
-		if platform.system().lower() == 'windows':
-			import ctypes
-			kernel32 = ctypes.windll.kernel32 # type: ignore[attr-defined]
-			kernel32.SetProcessWorkingSetSize(-1, ctypes.c_size_t(memory), ctypes.c_size_t(memory))
-		else:
-			import resource
-			resource.setrlimit(resource.RLIMIT_DATA, (memory, memory))
+def get_ui_component(name: ComponentName) -> Optional[Component]:
+	if name in UI_COMPONENTS:
+		return UI_COMPONENTS[name]
+	return None
 
 
-def pre_check() -> bool:
-	if sys.version_info < (3, 9):
-		update_status(wording.get('python_not_supported').format(version = '3.9'))
-		return False
-	if not shutil.which('ffmpeg'):
-		update_status(wording.get('ffmpeg_not_installed'))
-		return False
-	return True
+def register_ui_component(name: ComponentName, component: Component) -> None:
+	UI_COMPONENTS[name] = component
 
 
-def conditional_process() -> None:
-	for frame_processor_module in get_frame_processors_modules(facefusion.globals.frame_processors):
-		if not frame_processor_module.pre_process('output'):
-			return
-	if is_image(facefusion.globals.target_path):
-		process_image()
-	if is_video(facefusion.globals.target_path):
-		process_video()
+def launch() -> None:
+	with gradio.Blocks(theme = get_theme(), css = get_css(), title = metadata.get('name') + ' ' + metadata.get('version')) as ui:
+		for ui_layout in facefusion.globals.ui_layouts:
+			ui_layout_module = load_ui_layout_module(ui_layout)
+			if ui_layout_module.pre_render():
+				ui_layout_module.render()
+				ui_layout_module.listen()
+
+	for ui_layout in facefusion.globals.ui_layouts:
+		ui_layout_module = load_ui_layout_module(ui_layout)
+		ui_layout_module.run(ui)
 
 
-def process_image() -> None:
-	if predict_image(facefusion.globals.target_path):
-		return
-	shutil.copy2(facefusion.globals.target_path, facefusion.globals.output_path)
-	# process frame
-	for frame_processor_module in get_frame_processors_modules(facefusion.globals.frame_processors):
-		update_status(wording.get('processing'), frame_processor_module.NAME)
-		frame_processor_module.process_image(facefusion.globals.source_path, facefusion.globals.output_path, facefusion.globals.output_path)
-		frame_processor_module.post_process()
-	# compress image
-	update_status(wording.get('compressing_image'))
-	if not compress_image(facefusion.globals.output_path):
-		update_status(wording.get('compressing_image_failed'))
-	# validate image
-	if is_image(facefusion.globals.target_path):
-		update_status(wording.get('processing_image_succeed'))
-	else:
-		update_status(wording.get('processing_image_failed'))
+def get_theme() -> gradio.Theme:
+	return gradio.themes.Base(
+		primary_hue = gradio.themes.colors.red,
+		secondary_hue = gradio.themes.colors.neutral,
+		font = gradio.themes.GoogleFont('Open Sans')
+	).set(
+		background_fill_primary = '*neutral_100',
+		block_background_fill = 'white',
+		block_border_width = '0',
+		block_label_background_fill = '*primary_100',
+		block_label_background_fill_dark = '*primary_600',
+		block_label_border_width = 'none',
+		block_label_margin = '0.5rem',
+		block_label_radius = '*radius_md',
+		block_label_text_color = '*primary_500',
+		block_label_text_color_dark = 'white',
+		block_label_text_weight = '600',
+		block_title_background_fill = '*primary_100',
+		block_title_background_fill_dark = '*primary_600',
+		block_title_padding = '*block_label_padding',
+		block_title_radius = '*block_label_radius',
+		block_title_text_color = '*primary_500',
+		block_title_text_size = '*text_sm',
+		block_title_text_weight = '600',
+		block_padding = '0.5rem',
+		border_color_primary = 'transparent',
+		border_color_primary_dark = 'transparent',
+		button_large_padding = '2rem 0.5rem',
+		button_large_text_weight = 'normal',
+		button_primary_background_fill = '*primary_500',
+		button_primary_text_color = 'white',
+		button_secondary_background_fill = 'white',
+		button_secondary_border_color = 'transparent',
+		button_secondary_border_color_dark = 'transparent',
+		button_secondary_border_color_hover = 'transparent',
+		button_secondary_border_color_hover_dark = 'transparent',
+		button_secondary_text_color = '*neutral_800',
+		button_small_padding = '0.75rem',
+		checkbox_background_color = '*neutral_200',
+		checkbox_background_color_selected = '*primary_600',
+		checkbox_background_color_selected_dark = '*primary_700',
+		checkbox_border_color_focus = '*primary_500',
+		checkbox_border_color_focus_dark = '*primary_600',
+		checkbox_border_color_selected = '*primary_600',
+		checkbox_border_color_selected_dark = '*primary_700',
+		checkbox_label_background_fill = '*neutral_50',
+		checkbox_label_background_fill_hover = '*neutral_50',
+		checkbox_label_background_fill_selected = '*primary_500',
+		checkbox_label_background_fill_selected_dark = '*primary_600',
+		checkbox_label_text_color_selected = 'white',
+		input_background_fill = '*neutral_50',
+		shadow_drop = 'none',
+		slider_color = '*primary_500',
+		slider_color_dark = '*primary_600'
+	)
 
 
-def process_video() -> None:
-	if predict_video(facefusion.globals.target_path):
-		return
-	fps = detect_fps(facefusion.globals.target_path) if facefusion.globals.keep_fps else 25.0
-	# create temp
-	update_status(wording.get('creating_temp'))
-	create_temp(facefusion.globals.target_path)
-	# extract frames
-	update_status(wording.get('extracting_frames_fps').format(fps = fps))
-	extract_frames(facefusion.globals.target_path, fps)
-	# process frame
-	temp_frame_paths = get_temp_frame_paths(facefusion.globals.target_path)
-	if temp_frame_paths:
-		for frame_processor_module in get_frame_processors_modules(facefusion.globals.frame_processors):
-			update_status(wording.get('processing'), frame_processor_module.NAME)
-			frame_processor_module.process_video(facefusion.globals.source_path, temp_frame_paths)
-			frame_processor_module.post_process()
-	else:
-		update_status(wording.get('temp_frames_not_found'))
-		return
-	# merge video
-	update_status(wording.get('merging_video_fps').format(fps = fps))
-	if not merge_video(facefusion.globals.target_path, fps):
-		update_status(wording.get('merging_video_failed'))
-		return
-	# handle audio
-	if facefusion.globals.skip_audio:
-		update_status(wording.get('skipping_audio'))
-		move_temp(facefusion.globals.target_path, facefusion.globals.output_path)
-	else:
-		update_status(wording.get('restoring_audio'))
-		if not restore_audio(facefusion.globals.target_path, facefusion.globals.output_path):
-			update_status(wording.get('restoring_audio_failed'))
-			move_temp(facefusion.globals.target_path, facefusion.globals.output_path)
-	# clear temp
-	update_status(wording.get('clearing_temp'))
-	clear_temp(facefusion.globals.target_path)
-	# validate video
-	if is_video(facefusion.globals.target_path):
-		update_status(wording.get('processing_video_succeed'))
-	else:
-		update_status(wording.get('processing_video_failed'))
-
-
-def update_status(message : str, scope : str = 'FACEFUSION.CORE') -> None:
-	print('[' + scope + '] ' + message)
+def get_css() -> str:
+	fixes_css_path = resolve_relative_path('uis/assets/fixes.css')
+	overrides_css_path = resolve_relative_path('uis/assets/overrides.css')
+	return open(fixes_css_path, 'r').read() + open(overrides_css_path, 'r').read()
