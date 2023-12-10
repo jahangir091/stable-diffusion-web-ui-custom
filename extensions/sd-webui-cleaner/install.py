37376d0a152ec68babe6d4384d7f43baed43b5c8
jahangir <jahangir.cse09@gmail.com>
2023-12-10 18:40:38 +0600

    fixed installation of lama cleaner for server start optimization

--- extensions/sd-webui-cleaner/install.py
+++ extensions/sd-webui-cleaner/install.py
@@ -2,6 +2,6 @@ import launch
 
 # TODO: add pip dependency if need extra module only on extension
 
-# if not launch.is_installed("litelama"):
-#     launch.run_pip("install litelama==0.1.7")
+if not launch.is_installed("litelama"):
+    launch.run_pip("install litelama==0.1.7")
     
