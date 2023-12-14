b35928c631babc13ae5d44834adbf0bf531b520b
jahangir <jahangir.cse09@gmail.com>
2023-12-14 21:31:34 +0600

    added async with lama cleaner api and clean code

--- extensions/sd-webui-cleaner/scripts/api.py
+++ extensions/sd-webui-cleaner/scripts/api.py
@@ -14,28 +14,25 @@ import time
 def cleanup_api(_: gr.Blocks, app: FastAPI):
 
     @app.post("/sdwebui/ai/cleanup")
-    def clean_up(
+    async def clean_up(
         input_image: str = Body("", title='cleanup input image'),
         mask: str = Body("", title='clean up mask')
     ):
-
-        _image = api.decode_base64_to_image(input_image)
-        _mask = api.decode_base64_to_image(mask)
+        if not input_image or not mask:
+            return {"code": -1, "message": "input image and mask required"}
+        pil_image = api.decode_base64_to_image(input_image)
+        pil_mask = api.decode_base64_to_image(mask)
 
         start_time = time.time()
         utc_time = datetime.now(timezone.utc)
-        _output = lama.clean_object(_image, _mask)
-        output_img = api.encode_pil_to_base64(_output[0]).decode("utf-8")
-        
-        if len(_output) > 0:
-            return {"server_process_time": time.time()-start_time, "image": output_img, "server_hit_time": utc_time}
-        else:
+        output_images = lama.clean_object(pil_image, pil_mask)
+        if len(output_images) <= 0:
             return {"code": -1, "message": "Image generation failed"}
-
+        output_img = api.encode_pil_to_base64(output_images[0]).decode("utf-8")
+        return {"server_process_time": time.time()-start_time, "image": output_img, "server_hit_time": utc_time}
 
 try:
     import modules.script_callbacks as script_callbacks
-
     script_callbacks.on_app_started(cleanup_api)
 except:
     pass
